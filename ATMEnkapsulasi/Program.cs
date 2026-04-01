6cb908d773391ffb8f8a426d6cdaf8bfa739d3b0
atika <atika@biztwork.web.id>
2026-04-01 10:01:18 +0700

    Membuat set up awal class rekening

--- ATMEnkapsulasi/Program.cs
+++ ATMEnkapsulasi/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Rekening
+{
+    private string _noRekening;
+    private int _saldo;
+
+    public string NoRekening
+    {
+        set
+        {
+            _noRekening = value;
+        }
+        
+    }
+    public int Saldo
+        {
+         get { return _saldo; }
+         set
+         {
+            if (value < 0) Console.WriteLine("Saldo tidak boleh negatif!");
+            else _saldo = value;
+         }
+        }
+
+    public Rekening(string no_rekening)
+    {
+        NoRekening = no_rekening;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("menampilkan Info...");
+        Console.WriteLine($"Saldo saat ini {Saldo}");
+    }
+}
